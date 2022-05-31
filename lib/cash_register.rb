ef0b9471d82466fb9574af454e3d36d4fbc3f881
Luke Baltay <luke.baltay@yale.edu>
2022-05-31 15:17:03 -0400

    commit tree 3e6acf8e7ffc97549a22367794e128f4f292f0a9

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
 
+class CashRegister
+
+    def initialize(discount=0)
+        @total=0
+        @discount=discount
+    end
+
+    #def discount
+   #     @discount
+  #  end
+
+    attr_accessor :discount
+
+end
